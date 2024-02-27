ae0a4009ddda4a806c74c24cec1326adb6d63db7
Albin213 <85239240+Albin213@users.noreply.github.com>
2024-02-27 12:50:18 +0530

    Update page.js   Add pagination

--- src/app/admin/membersList/page.js
+++ src/app/admin/membersList/page.js
@@ -153,6 +153,7 @@ import Sidebar from '@/app/components/sidebar/sidebar';
 import MemberCard from '@/app/components/memberCard/MemberCard';
 import axios from 'axios';
 
+
 function MembersList() {
   
   const [viewMemberDetails, setViewMemberDetails] = useState(false);
@@ -161,19 +162,25 @@ function MembersList() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const [loggedIn, setLoggedIn] = useState("");
+  const [pageNo, setPageNo] = useState(1);
 
-  
+  console.log(pageNo);
+  console.log(typeof(pageNo));
+  console.log(membersData);
 
 
 
   useEffect(() => {
 
     setLoggedIn(localStorage.getItem("isLoggedIn"));
+    
 
     const fetchData = async () => {
       try {
         if(searchTerm === ""){
-          const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/admin/list-valid-users`);
+          const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/admin/list-valid-users`,{
+            page: pageNo
+          });
           setMembersData(response.data);
         }
       } catch (error) {
@@ -195,6 +202,8 @@ function MembersList() {
   }
 
   const handleSearch = async () => {
+
+    console.log(pageNo);
     try {
       const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/admin/search_users`, {
         search: searchTerm,
@@ -204,6 +213,40 @@ function MembersList() {
       console.error('Error fetching data:', error);
     }
   };
+
+
+  const handlePage = async () => {
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/admin/list-valid-users`, {
+        page: pageNo,
+      });
+      setMembersData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+
+  function handleNextPage() {
+    setPageNo(pageNo + 1);
+    handlePage();
+  }
+
+  function handlePreviousPage() {
+    if(pageNo > 1) {
+      setPageNo(pageNo - 1);
+      handlePage();
+    } else {
+      return;
+    }
+  }
+
+
+
+
+
+
 
   return (
     <>
@@ -219,7 +262,11 @@ function MembersList() {
         <div className="w-[20%] h-full bg-slate-950">
           <Sidebar />
         </div>
-        <div className="w-[80%] h-full p-8 flex flex-wrap overflow-y-auto">
+
+
+        <div className='w-[80%] h-full'>
+
+        <div className="w-full h-[90%] p-8 flex flex-wrap overflow-y-auto">
           <div className='w-[80%] h-[10%] mb-5 flex items-center'>
             <input
               type='text'
@@ -246,7 +293,29 @@ function MembersList() {
               handleCloseViewMemberDetails={handleCloseViewMemberDetails}
             />
           ))}
+
         </div>
+
+
+        <div className='w-full h-[10%] pe-[70px] flex flex-row-reverse items-center bg-[hsla(0,0%,0%,0.1)]'>
+              <button 
+                className='w-[160px] h-[40px] ms-4 bg-blue-950 text-white rounded  hover:bg-blue-700'
+                onClick= {handleNextPage}
+              >{"Next >>"}</button>
+
+              <button 
+                className='w-[160px] h-[40px] bg-blue-950 text-white rounded  hover:bg-blue-700'
+                onClick= {handlePreviousPage}
+              >{"<< Previous"}</button>
+        </div>
+
+
+        </div>
+
+        
+
+
+
       </div>
     </div>
       ) : (
